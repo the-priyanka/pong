9b20c0cfc27fcc09e7aef5d21f1d51965144a9cc
Priyanka <original.manaria@gmail.com>
2021-12-09 14:12:27 +0530

    create a requestAnimationFrame function

--- script.js
+++ script.js
@@ -50,12 +50,17 @@ const winningScore = 7;
 // Render Everything on Canvas
 function renderCanvas() {
   // Canvas Background
-  context.fillStyle = 'black';
+  context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
   // Paddle Color
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   // Player Paddle (Bottom)
-  context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+  context.fillRect(
+    paddleBottomX,
+    height - 20,
+    paddleWidth,
+    paddleHeight
+  );
   // Computer Paddle (Top)
   context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
   // Dashed Center Line
@@ -63,15 +68,15 @@ function renderCanvas() {
   context.setLineDash([4]);
   context.moveTo(0, 300);
   context.lineTo(500, 300);
-  context.strokeStyle = 'grey';
+  context.strokeStyle = "grey";
   context.stroke();
   // Ball
   context.beginPath();
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.fill();
   // Score
-  context.font = '32px Courier New';
+  context.font = "32px Courier New";
   context.fillText(playerScore, 20, canvas.height / 2 + 50);
   context.fillText(computerScore, 20, canvas.height / 2 - 30);
 }
@@ -83,9 +88,6 @@ function createCanvas() {
   body.appendChild(canvas);
   renderCanvas();
 }
-
-// Remove this
-createCanvas();
 
 // Reset Ball to Center
 function ballReset() {
@@ -202,6 +204,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -217,7 +220,6 @@ function startGame() {
   createCanvas();
   animate();
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
     playerMoved = true;
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
@@ -233,4 +235,4 @@ function startGame() {
 }
 
 // On Load
-// startGame();
+startGame();
